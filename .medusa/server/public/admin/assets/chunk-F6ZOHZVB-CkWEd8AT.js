4cee21ac0b864a7ed7856222a5ff0c6ac49ecc87
PabloGuzmanGIT <guzmagp23@gmail.com>
2025-03-29 13:20:35 -0500

    tratando de arreglar intento 2

--- .medusa/server/public/admin/assets/chunk-F6ZOHZVB-CkWEd8AT.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-CMAhTU0o.js";var e=({children:s})=>r.jsx("span",{className:"sr-only",children:s});export{e as V};
--- .medusa/server/public/admin/assets/home-KSB2J7CS-LiSKwELJ.js
+++
@@ -1,1 +0,0 @@
-import{u as r,r as t,j as s}from"./index-CMAhTU0o.js";var o=()=>{const e=r();return t.useEffect(()=>{e("/orders",{replace:!0})},[e]),s.jsx("div",{})};export{o as Component};
