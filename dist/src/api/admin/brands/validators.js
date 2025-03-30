bad89c44405135b2ac135304ad14759456192b1a
PabloGuzmanGIT <guzmagp23@gmail.com>
2025-03-29 23:31:50 -0500

    remove dist

--- dist/src/api/admin/brands/validators.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PostAdminCreateBrand = void 0;
-const zod_1 = require("zod");
-exports.PostAdminCreateBrand = zod_1.z.object({
-    name: zod_1.z.string(),
-});
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidmFsaWRhdG9ycy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uLy4uLy4uLy4uL3NyYy9hcGkvYWRtaW4vYnJhbmRzL3ZhbGlkYXRvcnMudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBQUEsNkJBQXVCO0FBRVYsUUFBQSxvQkFBb0IsR0FBRyxPQUFDLENBQUMsTUFBTSxDQUFDO0lBQzNDLElBQUksRUFBRSxPQUFDLENBQUMsTUFBTSxFQUFFO0NBQ2pCLENBQUMsQ0FBQSJ9
