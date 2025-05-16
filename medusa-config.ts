00e37cb08e34bae9ba974895a8003e0a0d0303da
PabloGuzmanGIT <guzmagp23@gmail.com>
2025-05-15 22:56:23 -0500

    link sync used

--- medusa-config.ts
+++ medusa-config.ts
@@ -18,6 +18,10 @@ module.exports = defineConfig({
 
   modules: [
     {
+      resolve: "@medusajs/stock-location",
+        options: {}
+    },
+    {
       resolve: "./src/modules/brand",
     }
   ]
